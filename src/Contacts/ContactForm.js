97bed9b0f1401b3e9c29fefae72f85624354f4b0
Kano066 <79617423+Kano066@users.noreply.github.com>
2022-05-08 18:22:58 -0600

    project update

---
+++ src/Contacts/ContactForm.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useForm, ValidationError} from '@formspree/react';
+import style from './Contacts.module.scss'
+import Fade from 'react-reveal/Fade'
+
+
+function ContactForm() {
+    const [state, handleSubmit] = useForm("mjvljvrz");
+    if (state.succeeded) {
+        return <p style={{color: 'white', fontSize: '14px', fontWeight: '700'}}>Thank you for contacting me!
+            I will get back to you as soon as possible.</p>
+    }
+    return (
+        <Fade left cascade>
+            <form onSubmit={handleSubmit} className={style.form}>
+                <input type="text" name="name" className={style.formArea} placeholder="Name"/>
+                <input type="email" name='email' className={style.formArea} placeholder="E-mail"/>
+                <ValidationError prefix="Email" field="email" errors={state.errors}/>
+                <textarea name={'message'} className={style.messageArea} placeholder="Your Message"/>
+                <ValidationError prefix="Message" field="message" errors={state.errors}/>
+                <button type="submit" disabled={state.submitting}>Send Message</button>
+            </form>
+        </Fade>
+    )
+}
+
+export default ContactForm;
--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -2,8 +2,10 @@ import React from 'react'
 import style from './Contacts.module.scss'
 import Title from "../common/components/title/Title"
 import Fade from 'react-reveal/Fade'
+import ContactForm from "./ContactForm";
 
 const Contacts = () => {
+
     return (
         <div id='contacts' className={style.contactBlock}>
             <div className={style.container}>
@@ -11,12 +13,16 @@ const Contacts = () => {
                     <Title text={'Contacts'}/>
                 </Fade>
                 <Fade left cascade>
-                    <form className={style.form}>
-                        <input type="text" className={style.formArea} placeholder="Name"/>
-                        <input type="text" className={style.formArea} placeholder="E-mail"/>
-                        <textarea className={style.messageArea} placeholder="Your Message"/>
-                        <button type="submit">Send Message</button>
-                    </form>
+                    {/*<form className={style.form}*/}
+                    {/*      action="https://formspree.io/f/mjvljvrz"*/}
+                    {/*      method="POST">*/}
+                    {/*    <input type="text" name="name" className={style.formArea} placeholder="Name"  />*/}
+                    {/*    <input type="email" name='email' className={style.formArea} placeholder="E-mail"/>*/}
+                    {/*    <textarea name={'message'} className={style.messageArea} placeholder="Your Message"/>*/}
+                    {/*    <button type="submit">Send Message</button>*/}
+                    {/*</form>*/}
+
+                    <ContactForm/>
                 </Fade>
             </div>
         </div>
