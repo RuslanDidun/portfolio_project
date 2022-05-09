56778a92a337b38c7d57d184356521b2a7f8103c
Kano066 <79617423+Kano066@users.noreply.github.com>
2022-05-08 19:25:35 -0600

    project update

--- src/MainInfoBlock/Main.js
+++ src/MainInfoBlock/Main.js
@@ -7,14 +7,20 @@ const Main = () => {
     return (
         <div id='main' className={style.mainBlock}>
             <div className={style.container}>
-
                 <div className={style.bgImage}/>
-
                 <div className={style.mainText}>
                     <Fade bottom>
                         <span> Hi There!</span>
                         <span>I am Ruslan <span>Didun</span></span>
                         <h1>Frontend Developer</h1>
+                    </Fade>
+                    <Fade bottom>
+                        <div className={style.quoteDiv}>
+                            <blockquote>
+                                " Simplicity is the soul of efficiency "
+                            </blockquote>
+                            <cite> ~ Austin Freeman</cite>
+                        </div>
                     </Fade>
                 </div>
             </div>
